40f989f9ceea62fa008828d5e02ee310d48bf955
이다운 <dlekdns07@plateer.com>
2025-07-31 10:03:59 +0900

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 import Link from 'next/link';
-import { useState, useEffect } from 'react';
+import { useEffect } from 'react';
 import toast from 'react-hot-toast';
 import {
     FiArrowRight,
